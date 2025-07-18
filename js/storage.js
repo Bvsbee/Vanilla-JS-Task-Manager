b254b1b2a3f727b4bd6bd1c9b43fcd1c458be713
Michael Busbee <busbee.michael101@gmail.com>
2025-07-17 21:33:19 -0400

    Created a Header.Created base for the form.

--- js/storage.js
+++ js/storage.js
@@ -1,1 +1,10 @@
 // Save and load task data from localStorage for persistence
+
+const task = {
+  title: "",
+  description: "",
+  createdAt: "",
+  completedAt: "",
+};
+
+localStorage.setItem("task", JSON.stringify(task));
